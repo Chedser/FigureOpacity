c182b3d7c3398a48d4f539f65ad174d009cdd245
Oleg Morozov <chedser@gmail.com>
2023-03-03 01:55:21 +0300

    Update Enums.cs

--- Assets/Resources/Scripts/Enums.cs
+++ Assets/Resources/Scripts/Enums.cs
@@ -10,13 +10,6 @@ public enum FigureType {
 
 //Уровень непрозрачности
 public enum Opacity { 
-  /*  ONE = 255,
-    TWO = 210,
-    THREE = 180,
-    FOUR = 100,
-    FIVE = 70,
-    NONE = -1*/
-
     ONE = 70,
     TWO = 100,
     THREE = 180,
